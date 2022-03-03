3a22fa76b5a489b0eb04e31923cc0afa782a9563
techdave <klatzdavie19@gmail.com>
2022-03-03 12:08:48 +0300

    add a UserList page component

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import UsersList from '../components/UsersList';
 
+const defaultImg = 'https://www.shutterstock.com/image-vector/default-avatar-profile-icon-grey-photo-518740741';
 const Users = () => {
-  return (
-    <div>Users</div>
-  )
-}
+  const USERS = [
+    { id: 'u1', name: 'David', image: defaultImg, places: 3 },
+    { id: 'u2', name: 'James', image: defaultImg, places: 6 },
+    { id: 'u3', name: 'Cate', image: defaultImg, places: 2 },
+  ];
 
-export default Users
+  return <UsersList items={USERS} />;
+};
+
+export default Users;
