2cf80cfc671625cf527b5fa087dedf21fd2de75e
techdave <klatzdavie19@gmail.com>
2022-03-03 13:02:45 +0300

    style the app and add more components

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,12 +1,16 @@
 import React from 'react';
+
 import UsersList from '../components/UsersList';
 
-const defaultImg = 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png';
 const Users = () => {
   const USERS = [
-    { id: 'u1', name: 'David', image: defaultImg, places: 3 },
-    { id: 'u2', name: 'James', image: defaultImg, places: 6 },
-    { id: 'u3', name: 'Cate', image: defaultImg, places: 2 },
+    {
+      id: 'u1',
+      name: 'Max Schwarz',
+      image:
+        'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      places: 3
+    }
   ];
 
   return <UsersList items={USERS} />;
